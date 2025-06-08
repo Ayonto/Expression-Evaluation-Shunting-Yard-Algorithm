d4d0d36db1f7eaaca19e2a18b8cd433d815dcc22
Shakibul Islam <58147322+Ayonto@users.noreply.github.com>
2025-06-08 12:08:40 +0600

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: A:/dev/Expression Evaluation-Shunting Yard Algorithm
-# Build directory: A:/dev/Expression Evaluation-Shunting Yard Algorithm/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
